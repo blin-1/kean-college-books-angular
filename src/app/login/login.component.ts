230461118108e5470780578f90090e81e960128a
blin-1 <y.monsoon@gmail.com>
2018-10-04 14:36:06 -0400

    Including gapi and gapi.auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl} from '@angular/forms';
 
 @Component({
+
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
@@ -30,7 +31,61 @@ export class LoginComponent implements OnInit {
   
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+	  
+	    var me = this;
+    	var auth2; 		// The Sign-In object.
+    	
+    	var appStart = function() {
+    	  gapi.load('auth2', initSigninV2);
+    	};
+
+    	var initSigninV2 = function() {
+    	  
+    	  auth2 = gapi.auth2.init({
+    	      client_id: '', //KeanBooks.classes.Credentials.OATH2_CLIENT_ID,
+    	      scope: 'profile'
+    	  });
+
+    	  // Listen for sign-in state changes.
+    	  auth2.isSignedIn.listen(signinChanged);
+
+    	  // Listen for changes to current user.
+    	  auth2.currentUser.listen(userChanged);
+
+    	  // Sign in the user if they are currently signed in.
+    	  if (auth2.isSignedIn.get() == true) {
+    	    auth2.signIn();
+    	  }
+    	  
+    	  // Start with the current live values.
+    	  refreshValues();
+    	  
+    	  // save reference   
+       	  //me.auth2 = auth2;
+       	  
+    	};
+
+    	var refreshValues = function() {
+      	  if (auth2){
+      	    ///            me.setModelState(auth2.isSignedIn.get());
+      	  }
+      	};
+    	
+    	var signinChanged = function (val) {
+    	 ///           me.setModelState(val);
+    	};
+
+    	var userChanged = function (user) {
+	  	   	if (auth2.isSignedIn.get()){
+	  	   		////   me.setModelState(true);
+		   	}
+    	};
+    	
+    	appStart();
+		
+	  
+  }
 
 
   onLogon() {	  
